c98db68ad87ff51fc7966555f3bf0b7c8613e47a
TsiryH <rajaonarivotsiry7@gmail.com>
2026-01-05 17:25:22 +0300

    ok

--- modal_project/eslint.config.mjs
+++ modal_project/eslint.config.mjs
@@ -5,6 +5,13 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  {
+    files: ["vitest.config.ts"],
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
