5ace3e478d3cf56c26aa910f68ba33f01d88245b
TsiryH <rajaonarivotsiry7@gmail.com>
2026-01-05 16:32:36 +0300

    Encore une fois

--- modal_project/src/components/utils/slot/slot.tsx
+++ modal_project/src/components/utils/slot/slot.tsx
@@ -1,23 +1,19 @@
 import * as React from "react"
 import { Slot as RadixSlot } from "@radix-ui/react-slot"
 
-type SlotProps<ElementType extends React.ElementType = "div"> = {
-  as?: ElementType
+type SlotProps = React.HTMLAttributes<HTMLElement> & {
+  as?: React.ElementType
   asChild?: boolean
-} & Omit<React.ComponentPropsWithRef<ElementType>, "as" | "asChild">
+}
 
-type SlotComponent = <ElementType extends React.ElementType = "div">(
-  props: SlotProps<ElementType>
-) => React.ReactElement | null
-
-const Slot = React.forwardRef<Element, SlotProps<any>>(function SlotInner(
-  { as, asChild, ...props }: SlotProps<any>,
+const Slot = React.forwardRef<HTMLElement, SlotProps>(function SlotInner(
+  { as, asChild, ...props },
   ref
 ) {
   const Component = asChild ? RadixSlot : (as ?? "div")
   return <Component ref={ref} {...props} />
-}) as unknown as SlotComponent
+})
 
-;(Slot as React.ForwardRefExoticComponent<React.RefAttributes<unknown>>).displayName = "Slot"
+Slot.displayName = "Slot"
 
 export { Slot }
